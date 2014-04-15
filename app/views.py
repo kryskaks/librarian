bf6578e84a56b6a348179e4d96afa50a50a8da1d
kryskaks <kryskaks@gmail.com>
2014-04-15 23:59:04 +0300

    Added search view

--- app/views.py
+++ app/views.py
@@ -1,19 +1,27 @@
 from flask import request, render_template, flash, g, session, redirect, url_for
-
 from app import db, app
-
 from app.models import Book
+from app.forms import SearchForm
+
 
 @app.route('/hello')
 def home():
     return render_template('hello.html', hello_text = 'Hello World!')
 
 @app.route('/')
+def index():
+    return render_template('index.html', form = SearchForm())
+
+@app.route('/books')
 def books_list():
 	books = Book.query.all()
-	if books:
-		return ','.join([str(b) for b in books])
-	return 'List is empty'
+	return render_template('books.html', books = books)
+
+@app.route('/search', methods = ["POST"])
+def search():	
+	form = SearchForm(request.form)		
+	books = Book.query.filter_by(title = form.text.data)
+	return render_template('search_result.html', books = books)
 
 @app.route('/add/book')
 def add_book(title):
